66317263eb1257f4ace8f39224c3e55e38185cec
Michael Gerlach <mgerlach0130@gmail.com>
2024-03-06 21:18:25 -0600

    put together some basic test functionality for rendering product cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,52 @@ const makeUpBtn = document.getElementById('makeupbtn');
 const hairCareBtn = document.getElementById('haircarebtn');
 const earringsBtn = document.getElementById('earringsbtn');
 const allProductsBtn = document.getElementById('allproductsbtn');
+const header = document.getElementById('productheader');
+const container = document.getElementById('container');
+const card = `<style>
+            img {
+                height: 200px;
+                width: 50px;
+            }
+            h4, p {
+                text-align: center;
+            }
+            .card {
+                margin: auto;
+            }
+            </style>
+            <div class="card">
+            <img src="./assets/images/amikatheshield.jpg" style="width:100%">
+            <div class="innercard">
+            <h4><b>Amika The Shield</b></h4>
+            <p>$30</p>
+            </div>
+            </div>
+            <div class="card">
+            <img src="./assets/images/brazilianblowoutantifrizzconditioner.png" style="width:100%">
+            <div class="innercard">
+            <h4><b>Brazilian Blowout Anti Frizz Conditioner</b></h4>
+            <p>$36</p>
+            </div>
+            </div>`
+
 
 function showMakeUp() {
-    console.log('makeup');
+    header.innerHTML = `Showing Make Up Products`;
+    container.innerHTML = "";
 };
 
 function showHairCare() {
-    console.log('hair care');
+    header.innerHTML = `Showing Hair Care Products`;
+    container.innerHTML = card;
 };
 
 function showEarrings() {
-    console.log('earrings');
+    header.innerHTML = `Showing Current Earrings`;
 };
 
 function showAllProducts() {
-    console.log('all products');
+    header.innerHTML = `Showing All Products`;
 };
 
 makeUpBtn.addEventListener('click', showMakeUp);
